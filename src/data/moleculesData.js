a69da2548072135e1fe1b8e066eb8cf6c6f855dc
Eros De Simone <eros.desimone@yahoo.it>
2025-11-12 14:17:14 +0100

    ok dati nuovi

--- src/data/moleculesData.js
+++ src/data/moleculesData.js
@@ -1,36 +1,5 @@
 // SMILES di molecole reali comuni
 export const moleculeSMILES = [
-  "CC(=O)OC1=CC=CC=C1C(=O)O", // Aspirina
-  "CN1C=NC2=C1C(=O)N(C(=O)N2C)C", // Caffeina
-  "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O", // Ibuprofene
-  "CC(=O)NC1=CC=C(C=C1)O", // Paracetamolo
-  "C1=CC=C(C=C1)C=O", // Benzaldeide
-  "CC(C)NCC(COC1=CC=CC=C1)O", // Propranololo
-  "CN(C)CCC=C1C2=CC=CC=C2CCC3=CC=CC=C13", // Amitriptilina
-  "CC1=CC=C(C=C1)C(=O)O", // Acido p-toluico
-  "C1=CC=C2C(=C1)C(=CN2)CCN", // Triptamina
-  "CC(C)CC1=CC=C(C=C1)C(C)C", // p-Cimene
-  "COC1=CC=CC=C1O", // Guaiacolo
-  "C1=CC(=CC=C1O)O", // Idrochinone
-  "C1=CC=C(C=C1)N", // Anilina
-  "CC(C)C1=CC=CC=C1", // Cumene
-  "C1=CC=C(C=C1)Cl", // Clorobenzene
-  "CC1=CC=CC=C1C", // o-Xilene
-  "C1=CC=C(C=C1)C(=O)C2=CC=CC=C2", // Benzofenone
-  "C1=CC=C(C=C1)C#N", // Benzonitrile
-  "CC1=CC=C(C=C1)N(=O)=O", // p-Nitrotoluene
-  "C1=CC=C(C=C1)S", // Tiofenolo
-  "CC(C)(C)C1=CC=C(C=C1)O", // 4-tert-Butilfenolo
-  "C1=CC=C(C=C1)C(=O)OC", // Benzoato di metile
-  "CC1=CC=C(C=C1)C=C", // p-Metilstirene
-  "C1=CC=C(C=C1)CC(=O)O", // Acido fenilacetico
-  "CC(C)OC(=O)C1=CC=CC=C1", // Benzoato di isopropile
-  "C1=CC=C2C(=C1)C=CC=C2", // Naftalene
-  "CC1=CC2=C(C=C1)C=CC=C2", // 2-Metilnaftalene
-  "C1=CC=C(C=C1)COC2=CC=CC=C2", // Benzil fenil etere
-  "CC(C)C1=CC=C(C=C1)CC(C)C", // p-Diisopropilbenzene
-  "C1=CC=C(C=C1)C(C)(C)C", // tert-Butilbenzene
-
   // Farmaci complessi
   "CC1=C(C(=O)N(C2=CC=CC=C12)C3=CC=CC=C3C(=O)O)CC(=O)O", // Indometacina
   "CN1C2CCC1CC(C2)OC(=O)C(CO)C3=CC=CC=C3", // Atropina
@@ -82,6 +51,39 @@ export const moleculeSMILES = [
   "CC1=C(C(=NO1)C2=CC=CC=C2)C(=O)NC3=CC=C(C=C3)C#N", // Isossazolo nitrile
   "C1=CC=C(C=C1)C2=CC(=NO2)C3=CC=C(C=C3)Cl", // Ossazolo clorurato
 ];
+
+{/*
+  "CC(=O)OC1=CC=CC=C1C(=O)O", // Aspirina
+  "CN1C=NC2=C1C(=O)N(C(=O)N2C)C", // Caffeina
+  "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O", // Ibuprofene
+  "CC(=O)NC1=CC=C(C=C1)O", // Paracetamolo
+  "C1=CC=C(C=C1)C=O", // Benzaldeide
+  "CC(C)NCC(COC1=CC=CC=C1)O", // Propranololo
+  "CN(C)CCC=C1C2=CC=CC=C2CCC3=CC=CC=C13", // Amitriptilina
+  "CC1=CC=C(C=C1)C(=O)O", // Acido p-toluico
+  "C1=CC=C2C(=C1)C(=CN2)CCN", // Triptamina
+  "CC(C)CC1=CC=C(C=C1)C(C)C", // p-Cimene
+  "COC1=CC=CC=C1O", // Guaiacolo
+  "C1=CC(=CC=C1O)O", // Idrochinone
+  "C1=CC=C(C=C1)N", // Anilina
+  "CC(C)C1=CC=CC=C1", // Cumene
+  "C1=CC=C(C=C1)Cl", // Clorobenzene
+  "CC1=CC=CC=C1C", // o-Xilene
+  "C1=CC=C(C=C1)C(=O)C2=CC=CC=C2", // Benzofenone
+  "C1=CC=C(C=C1)C#N", // Benzonitrile
+  "CC1=CC=C(C=C1)N(=O)=O", // p-Nitrotoluene
+  "C1=CC=C(C=C1)S", // Tiofenolo
+  "CC(C)(C)C1=CC=C(C=C1)O", // 4-tert-Butilfenolo
+  "C1=CC=C(C=C1)C(=O)OC", // Benzoato di metile
+  "CC1=CC=C(C=C1)C=C", // p-Metilstirene
+  "C1=CC=C(C=C1)CC(=O)O", // Acido fenilacetico
+  "CC(C)OC(=O)C1=CC=CC=C1", // Benzoato di isopropile
+  "C1=CC=C2C(=C1)C=CC=C2", // Naftalene
+  "CC1=CC2=C(C=C1)C=CC=C2", // 2-Metilnaftalene
+  "C1=CC=C(C=C1)COC2=CC=CC=C2", // Benzil fenil etere
+  "CC(C)C1=CC=C(C=C1)CC(C)C", // p-Diisopropilbenzene
+  "C1=CC=C(C=C1)C(C)(C)C", // tert-Butilbenzene 
+  */}
 
 // Funzione per ottenere N molecole casuali uniche
 export function getRandomMolecules(count = 25) {
